c9e49c645deb43eff7eebb7801ee7353e95dd6c3
dvnamm1605 <dauvannam001@gmail.com>
2025-08-12 17:36:06 +0700

    use best model test

--- test.py
+++ test.py
@@ -5,26 +5,6 @@ import cv2
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
-
-all_images = glob.glob("data/archive/images/train/*.jpg")
-
-
-test_images = random.sample(all_images, 6)
-
-fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-
-for img_path, ax in zip(test_images, axes.flatten()):
-    results = model(img_path, device=0)      #  YOLO 
-    result_img = results[0].plot() # annotated 
-    
-    img_rgb = cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB)
-    ax.imshow(img_rgb)
-    ax.axis("off")
-    ax.set_title(os.path.basename(img_path))  
-
-plt.tight_layout()
-plt.show()
-
 TRAINED_MODEL_PATH = 'runs/detect/train3/weights/best.pt' 
 
 # Kiểm tra xem file có tồn tại không
